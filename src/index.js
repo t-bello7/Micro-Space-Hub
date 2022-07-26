0d6fcd67e299c58c51434edc1c9c9eb64c79158b
shaqdeff <shaquillendunda@gmail.com>
2022-07-26 07:22:57 +0300

    Rectify eslint error

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+/* eslint-disable comma-dangle */
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-
