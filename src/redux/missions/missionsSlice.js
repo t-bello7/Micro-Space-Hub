f90a6fa800e0dd2a482ca7109dfdf37ca0d79513
shaqdeff <shaquillendunda@gmail.com>
2022-07-27 01:49:46 +0300

    Setup missions slice

---
+++ src/redux/missions/missionsSlice.js
@@ -0,0 +1,10 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = [];
+
+const missionsSlice = createSlice({
+  name: 'missions',
+  initialState,
+});
+
+export default missionsSlice.reducer;
