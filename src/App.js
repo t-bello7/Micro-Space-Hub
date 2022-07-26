71ea1cc9e53a283cc138c4e4833ebd6aeb6c44f5
shaqdeff <shaquillendunda@gmail.com>
2022-07-26 07:08:32 +0300

    Add active link styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavLink from './components/NavLink';
+import Navbar from './components/Navbar';
 import Rockets from './components/Rockets';
 import Missions from './components/Missions';
 import MyProfile from './components/MyProfile';
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <NavLink />
+        <Navbar />
         <Routes>
           <Route path="/" element={<Rockets />} />
           <Route path="/missions" element={<Missions />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,31 @@
-import { Link } from 'react-router-dom';
-import './NavLink.css';
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
 
 const image = require('../assets/planet.png');
 
-function NavLink() {
+function Navbar() {
   return (
     <nav className="nav-bar">
       <div className="logo-container">
         <img className="planet-img" src={image} alt="planet" />
         <h1 className="logo-name">
-          <Link to="/">Space Travelers&apos; Hub</Link>
+          <NavLink to="/">Space Travelers&apos; Hub</NavLink>
         </h1>
       </div>
       <ul className="nav-list">
         <li className="nav-item">
-          <Link to="/">Rockets</Link>
+          <NavLink to="/">Rockets</NavLink>
         </li>
         <li className="nav-item">
-          <Link to="/missions">Missions</Link>
+          <NavLink to="/missions">Missions</NavLink>
         </li>
         <div className="vertical-line" />
         <li className="nav-item">
-          <Link to="/my-profile">My Profile</Link>
+          <NavLink to="/my-profile">My Profile</NavLink>
         </li>
       </ul>
     </nav>
   );
 }
 
-export default NavLink;
+export default Navbar;
