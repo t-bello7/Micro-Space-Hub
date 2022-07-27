768607ad25a7b0f9db1c40c5331185c56038e834
shaqdeff <shaquillendunda@gmail.com>
2022-07-28 01:56:10 +0300

    Set up profile component

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -1,5 +1,19 @@
+/* eslint-disable object-curly-newline */
+import { Container, Row, Col, ListGroup } from 'react-bootstrap';
+
 function MyProfile() {
-  return <div>My Profile</div>;
+  return (
+    <div>
+      <Container className="profile-container">
+        <Row>
+          <Col>
+            <h4>My Profile</h4>
+            <ListGroup />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default MyProfile;
