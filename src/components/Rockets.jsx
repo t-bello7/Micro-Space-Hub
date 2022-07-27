8bb1eca7fd36c952d2b2b9b8edd1079294cd1c4d
shaqdeff <shaquillendunda@gmail.com>
2022-07-28 01:47:04 +0300

    Enhance styling

--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -1,25 +1,23 @@
 /* eslint-disable quotes */
 /* eslint-disable arrow-body-style */
-
-import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
-import Button from "react-bootstrap/Button";
-import ListGroup from "react-bootstrap/ListGroup";
-import Badge from "react-bootstrap/Badge";
-import Image from "react-bootstrap/Image";
+/* eslint-disable react/jsx-closing-bracket-location */
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Badge from 'react-bootstrap/Badge';
+import Image from 'react-bootstrap/Image';
 import {
   selectAllRockets,
   fetchRockets,
   reserveRockets,
   cancelRockets,
-} from "../redux/rockets/rocketSlice";
-import "./Rockets.css";
+} from '../redux/rockets/rocketSlice';
+import './Rockets.css';
 
 function Rockets() {
   const dispatch = useDispatch();
-  const { data: rockets, status } = useSelector(
-    selectAllRockets
-  );
+  const { data: rockets, status } = useSelector(selectAllRockets);
 
   const handleReserveButton = (id) => {
     dispatch(reserveRockets(id));
@@ -30,22 +28,16 @@ function Rockets() {
   };
 
   useEffect(() => {
-    if (status === "idle") {
+    if (status === 'idle') {
       dispatch(fetchRockets());
     }
   }, [status, dispatch]);
 
   return (
-    <ListGroup
-      className="rocket__container"
-      variant="flush"
-    >
+    <ListGroup className="rocket__container" variant="flush">
       {rockets.map((rocket) => {
         return (
-          <ListGroup.Item
-            key={rocket.id}
-            className="mt-5 d-flex"
-          >
+          <ListGroup.Item key={rocket.id} className="mt-5 py-4 d-flex">
             <Image
               src={rocket.flickr_images[0]}
               alt="rocket-pic"
@@ -53,33 +45,46 @@ function Rockets() {
             />
             <div className="mx-4">
               {rocket.reserved ? (
-                <Badge bg="success"> Reserved </Badge>
+                <Badge
+                  bg="success"
+                  style={{
+                    fontSize: '1.15rem',
+                    padding: '0.5rem 1rem',
+                    marginBottom: '0.5rem',
+                  }}>
+                  Reserved
+                </Badge>
               ) : (
-                ""
+                ''
               )}
               <h2 className="h1">{rocket.rocket_name}</h2>
-              <p className="mt-4 lead">
+              <p
+                className="mt-4 lead"
+                style={{ fontSize: '1.4rem', fontWeight: '400' }}>
                 {rocket.description}
               </p>
 
               {rocket.reserved ? (
                 <Button
-                  variant="light"
-                  className="btn-lg"
-                  onClick={() =>
-                    handleCancelButton(rocket.id)
-                  }
-                >
+                  variant="outline-danger btn-lg"
+                  onClick={() => handleCancelButton(rocket.id)}
+                  style={{
+                    fontWeight: '600',
+                    fontSize: '1.3rem',
+                    padding: '0.75rem 1rem',
+                  }}>
                   Cancel Reservation
                 </Button>
               ) : (
                 <Button
                   className="btn-lg"
-                  onClick={() =>
-                    handleReserveButton(rocket.id)
-                  }
-                >
-                  Reserve Reservation
+                  onClick={() => handleReserveButton(rocket.id)}
+                  style={{
+                    fontWeight: '600',
+                    fontSize: '1.3rem',
+                    padding: '0.75rem 1rem',
+                  }}>
+                  Reserve Rocket
                 </Button>
               )}
             </div>
