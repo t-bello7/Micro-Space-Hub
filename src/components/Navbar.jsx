8cde64bc4cfd6584ecca40d528e81d24d1fa3f27
Bello Oluwatomisin <bello4aus@gmail.com>
2022-08-31 23:16:18 +0100

    Deploy to production (#44)
    * Update README.md
    
    * Fix deploy (#38)
    
    * fix deploy issues
    
    * fix deploy issues
    
    * change favicon icon
    
    * add responsiveness to project (#40)
    
    * Release to Production 1 (#39) (#41)
    
    * Update README.md
    
    * Fix deploy (#38)
    
    * fix deploy issues
    
    * fix deploy issues
    
    * change favicon icon
    
    Co-authored-by: Shaquille Ndunda <92020713+shaqdeff@users.noreply.github.com>
    
    Co-authored-by: Shaquille Ndunda <92020713+shaqdeff@users.noreply.github.com>
    
    * Responsive  (#42)
    
    * add responsiveness to project
    
    * fix navbar width
    
    * Release to Production 1 (#39) (#43)
    
    * Update README.md
    
    * Fix deploy (#38)
    
    * fix deploy issues
    
    * fix deploy issues
    
    * change favicon icon
    
    Co-authored-by: Shaquille Ndunda <92020713+shaqdeff@users.noreply.github.com>
    
    Co-authored-by: Shaquille Ndunda <92020713+shaqdeff@users.noreply.github.com>
    
    Co-authored-by: Shaquille Ndunda <92020713+shaqdeff@users.noreply.github.com>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,17 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
 const image = require('../assets/planet.png');
 
 function Navbar() {
+  const [navOpen, setNavOpen] = useState(false);
+  const handleToggle = () => {
+    setNavOpen((prev) => !prev);
+  };
+  const closeMenu = () => {
+    setNavOpen(false);
+  };
   return (
     <nav className="nav-bar">
       <div className="logo-container">
@@ -12,16 +20,22 @@ function Navbar() {
           <NavLink to="/">Space Travelers&apos; Hub</NavLink>
         </h1>
       </div>
-      <ul className="nav-list">
+      <button type="button" className="btn-menu close" onClick={handleToggle}>
+        <img src="/menu.svg" alt="menu-icon" />
+      </button>
+      <ul className={`nav-list ${navOpen ? "" : "hide"}`}>
+        <button type="button" className="btn-menu close" onClick={closeMenu}>
+          <img src="/close.svg" alt="menu-icon" />
+        </button>
         <li className="nav-item">
-          <NavLink to="/">Rockets</NavLink>
+          <NavLink to="/" onClick={closeMenu}>Rockets</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/missions">Missions</NavLink>
+          <NavLink to="/missions" onClick={closeMenu}>Missions</NavLink>
         </li>
         <div className="vertical-line" />
         <li className="nav-item">
-          <NavLink to="/my-profile">My Profile</NavLink>
+          <NavLink to="/my-profile" onClick={closeMenu}>My Profile</NavLink>
         </li>
       </ul>
     </nav>
